ea6691a4e3d85a0fad5846643336842495ec169f
Rubrical <filipesalviano@proton.me>
2024-02-07 23:17:32 -0300

    First Commit

---
+++ src/main/java/com/senac/solucaocontabil/SolucaoContabilApplication.java
@@ -0,0 +1,13 @@
+package com.senac.solucaocontabil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolucaoContabilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolucaoContabilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/solucaocontabil/SolucaoContabilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.solucaocontabil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolucaoContabilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
